5391579846c3fad5a4b4daa7391cdd14909e26b3
Sylvain Thibault <sthibault63@gmail.com>
2018-03-05 15:44:09 +0100

    mise en place des routes

--- vuejs-park/src/App.vue
+++ vuejs-park/src/App.vue
@@ -1,76 +1,79 @@
 <template>
-  <div id="app">
-    <img src="http://vuejs.org/images/logo.png">
-    <h1>{{ msg }}</h1>
-    <div class="inline-button" @click="onMachinesListClick">Consulter la liste des machines</div>
-    <div class="inline-button" @click="onMapClick">Voir la carte</div>
-    <machines-list></machines-list>
-    <machines-map></machines-map>
-  </div>
+    <div id="app">
+        <img src="http://vuejs.org/images/logo.png">
+        <h1>{{ msg }}</h1>
+        <div class="inline-button" @click="onMachinesListClick"><router-link to="/list"> Consulter la liste des machines </router-link></div>
+        <div class="inline-button" @click="onMapClick"><router-link to="/map"> Voir la carte </router-link></div>
+        <router-view> </router-view>
+    </div>
 </template>
 
 <script>
-import MachinesList from "./MachinesList.vue";
-import MachinesMap from "./MachinesMap.vue";
+    import MachinesList from "./MachinesList.vue";
+    import MachinesMap from "./MachinesMap.vue";
 
-export default {
-    components: {
-        MachinesMap,
-        MachinesList},
-    name: 'app',
-  data () {
-    return {
-      msg: "Qu'est-ce tu veux faire gros ?"
-    }
-  },
-    methods: {
-        onMachinesListClick: function(){
-            window.alert('machines');
+    export default {
+        name: 'app',
+        components: {
+            MachinesMap,
+            MachinesList
         },
-        onMapClick: function() {
-            window.alert('maps')
-        }
-    },
-}
+
+        data () {
+            return {
+                msg: "Qu'est-ce tu veux faire gros ?"
+            }
+        },
+
+        methods: {
+            onMachinesListClick: function(){
+                //window.alert('machines');
+            },
+            onMapClick: function() {
+                //window.alert('maps')
+            }
+        },
+    }
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-img {
-  width: 200px;
-  height: 200px;
-}
+    img {
+        width: 200px;
+        height: 200px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
 
-.inline-button {
-    display: inline-block;
-    background-color: #14b776;
-    color: white;
-    padding: 20px 40px;
+    .inline-button {
+        display: inline-block;
+        background-color: #14b776;
+        color: white;
+        padding: 20px 40px;
     }
 </style>
--- vuejs-park/src/main.js
+++ vuejs-park/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+import MachinesList from "./MachinesList.vue";
+import MachinesMap from "./MachinesMap.vue";
+
+const routes = [
+    {path: '/list' , component: MachinesList},
+    {path: '/map' , component: MachinesMap}
+];
+
+const router = new VueRouter({
+    routes // raccourci pour `routes: routes`
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
